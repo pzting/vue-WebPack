dab180dba3ac7d381b87b1bf20391af1980dc851
1612750269@qq.com <1612750269@qq.com>
2018-11-02 17:29:43 +0800

    vue+webpack

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">{{text}}</div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                text: 'hello world'
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
